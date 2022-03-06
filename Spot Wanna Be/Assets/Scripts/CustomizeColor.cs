9addbf5a31781f8b2f34dcb88162d8a3fd679d87
Dionovan Ramadhani <80899280+nyanboy1127@users.noreply.github.com>
2022-03-06 18:41:44 +0700

    change color mechanics

---
+++ Spot Wanna Be/Assets/Scripts/CustomizeColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizeColor : MonoBehaviour
+{
+    Renderer rend;
+
+    private void Start() {
+        rend = GetComponent<Renderer>();
+    }
+
+    public void ChangeColor(Material mat) {
+        rend.sharedMaterial = mat;
+    }
+
+}
