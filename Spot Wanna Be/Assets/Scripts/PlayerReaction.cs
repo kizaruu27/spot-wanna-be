460234ab707f5b0c30f059d87312d72a3f153947
Dionovan Ramadhani <80899280+nyanboy1127@users.noreply.github.com>
2022-03-14 07:16:52 +0700

    edit item stop

---
+++ Spot Wanna Be/Assets/Scripts/PlayerReaction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerReaction : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    public void PlayerDance()
+    {
+        anim.SetTrigger("Dance");
+    }
+}
